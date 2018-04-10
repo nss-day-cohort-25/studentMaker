a948180e26125bb33f3612b95d14d85935b83a1d
Steve Brownlee <chortlehoort@gmail.com>
2018-04-10 15:06:26 -0500

    Initial commit

---
+++ maker.js
@@ -0,0 +1,27 @@
+const studentBodyRef = document.querySelector("#studentBody")
+
+const StudentDatabase = JSON.parse(localStorage.getItem("StudentDatabase"))
+
+// Array of student objects
+StudentDatabase.students.forEach(
+    currentStudent => {
+        // Build the DOM components
+
+        // Section first
+        const studentSection = document.createElement("section")
+
+        // h2 child component of section
+        const studentName = document.createElement("h2")
+        studentName.textContent = currentStudent.name
+        studentSection.appendChild(studentName)
+
+        // p child component of section
+        const studentBP = document.createElement("p")
+        studentBP.textContent = currentStudent.birthPlace
+        studentSection.appendChild(studentBP)
+
+        // Append HTML representation of student to the DOM
+        studentBodyRef.appendChild(studentSection)
+    }
+)
+
